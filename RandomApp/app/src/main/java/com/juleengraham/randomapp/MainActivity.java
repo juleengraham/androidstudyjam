509ba24dddee4cc631a43b89eb1b3a0a6498d56a
Juleen Graham <jugraham@vassar.edu>
2016-03-16 16:28:33 -0400

    No Override OnCreate method
    What happens when there is no @Override OnCreate method in the
    MainActivity class? The screen is blank.

--- RandomApp/app/src/main/java/com/juleengraham/randomapp/MainActivity.java
+++ RandomApp/app/src/main/java/com/juleengraham/randomapp/MainActivity.java
@@ -7,15 +7,4 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        TextView textView = new TextView(this);
-
-        textView.setText("hskdhkja");
-        textView.setTextSize(40);
-        textView.setTextColor(Color.parseColor("#060e9f"));
-
-        setContentView(textView);
-    }
 }
