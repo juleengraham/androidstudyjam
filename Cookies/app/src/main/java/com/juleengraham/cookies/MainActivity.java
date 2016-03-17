08b4f4fe68d92928edc0716cbd78e4688520d63b
Juleen Graham <jugraham@vassar.edu>
2016-03-16 23:51:59 -0400

    Practice manipulating views

---
+++ Cookies/app/src/main/java/com/juleengraham/cookies/MainActivity.java
@@ -0,0 +1,30 @@
+package com.juleengraham.cookies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.juleengraham.cookies.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        ImageView cookieImageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        cookieImageView.setImageResource(R.drawable.after_cookie);
+
+        TextView statusTextView = (TextView) findViewById(R.id.status_text_view);
+        statusTextView.setText("I'm so full");
+    }
+}
